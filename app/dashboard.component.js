df59264ab59747862e7ce688fb80ec5bca695ed5
cprater <cprater23@gmail.com>
2016-02-29 08:26:45 -0700

    finished dat tutorial

--- app/dashboard.component.js
+++ app/dashboard.component.js
@@ -11,7 +11,7 @@ System.register(['angular2/core', 'angular2/router', './hero.service'], function
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
     var core_1, router_1, hero_service_1;
-    var HeroDetailComponent;
+    var DashboardComponent;
     return {
         setters:[
             function (core_1_1) {
@@ -24,33 +24,33 @@ System.register(['angular2/core', 'angular2/router', './hero.service'], function
                 hero_service_1 = hero_service_1_1;
             }],
         execute: function() {
-            HeroDetailComponent = (function () {
-                function HeroDetailComponent(_heroService, _routeParams) {
+            DashboardComponent = (function () {
+                function DashboardComponent(_heroService, _router) {
                     this._heroService = _heroService;
-                    this._routeParams = _routeParams;
+                    this._router = _router;
+                    this.heroes = [];
                 }
-                HeroDetailComponent.prototype.ngOnInit = function () {
+                DashboardComponent.prototype.ngOnInit = function () {
                     var _this = this;
-                    var id = +this._routeParams.get('id');
-                    this._heroService.getHero(id)
-                        .then(function (hero) {
-                        _this.hero = hero;
+                    this._heroService.getHeroes()
+                        .then(function (heroes) {
+                        _this.heroes = heroes.slice(1, 5);
                     });
                 };
-                HeroDetailComponent.prototype.goBack = function () {
-                    window.history.back();
+                DashboardComponent.prototype.gotoDetail = function (hero) {
+                    var link = ['HeroDetail', { id: hero.id }];
+                    this._router.navigate(link);
                 };
-                HeroDetailComponent = __decorate([
+                DashboardComponent = __decorate([
                     core_1.Component({
-                        selector: 'my-hero-detail',
-                        template: "\n    <div *ngIf=\"hero\">\n      <h2>{{hero.name}} details!</h2>\n      <div><label>id: </label>{{hero.id}}</div>\n      <div>\n        <label>name: </label>\n        <input [(ngModel)]=\"hero.name\" placeholder=\"name\"/>\n      </div>\n    </div>\n  ",
-                        inputs: ['hero']
+                        selector: 'my-dashboard',
+                        templateUrl: 'app/dashboard.component.html',
                     }), 
-                    __metadata('design:paramtypes', [hero_service_1.HeroService, router_1.RouteParams])
-                ], HeroDetailComponent);
-                return HeroDetailComponent;
+                    __metadata('design:paramtypes', [hero_service_1.HeroService, router_1.Router])
+                ], DashboardComponent);
+                return DashboardComponent;
             }());
-            exports_1("HeroDetailComponent", HeroDetailComponent);
+            exports_1("DashboardComponent", DashboardComponent);
         }
     }
 });
--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,4 +1,6 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
+import {RouteParams} from 'angular2/router';
+import {HeroService} from './hero.service';
 import {Hero} from './hero';
 
 @Component({
@@ -18,4 +20,22 @@ import {Hero} from './hero';
 
 export class HeroDetailComponent {
   hero: Hero;
+
+  constructor(
+    private _heroService: HeroService,
+    private _routeParams: RouteParams
+  ) {}
+
+  ngOnInit() {
+    let id = +this._routeParams.get('id');
+
+    this._heroService.getHero(id)
+      .then(hero => {
+        this.hero = hero;
+      });
+  }
+
+  goBack() {
+    window.history.back();
+  }
 }
